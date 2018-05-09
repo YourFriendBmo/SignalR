1dd73d0abfa4ec74f1fc07ac03b0d3ed939ed8d7
BrennanConroy <brecon@microsoft.com>
2018-05-09 12:10:54 -0700

    NTP

--- benchmarkapps/BenchmarkServer/Hubs/EchoHub.cs
+++ benchmarkapps/BenchmarkServer/Hubs/EchoHub.cs
@@ -45,5 +45,10 @@ namespace BenchmarkServer.Hubs
         {
             // Dump the payload, we don't care
         }
+
+        public DateTime NTP()
+        {
+            return DateTime.UtcNow;
+        }
     }
 }
